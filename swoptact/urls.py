12e801a8a1828fc249942bd75d62590fac80a271
Jessica Tallon <jessica@megworld.co.uk>
2015-02-26 16:26:55 +0100

    Mount admin UI at base of URL and remove unneeded templates and views

--- swoptact/urls.py
+++ swoptact/urls.py
@@ -22,7 +22,5 @@ from django.conf.urls import patterns, include, url
 from .views import index
 
 urlpatterns = patterns("",
-    url(r"^$", index.LoginView.as_view(), name="home"),
-    url(r"^success/", index.SuccessView.as_view(), name="success"),
-    url(r"^admin/", include(admin.site.urls)),
+    url(r"^", include(admin.site.urls)),
 )
