5bbe2e16c9fc98c60d75c8e5763149755eac1b2c
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2015-03-24 08:31:14 -0500

    Add (BSD) copyright headers to django_google_maps app

--- django_google_maps/widgets.py
+++ django_google_maps/widgets.py
@@ -1,6 +1,15 @@
-# No copyright header was originally placed in this file, but it was
-# sourced from Aaron Madison in 2015.  See
-# https://github.com/madisona/django-google-maps
+# Copyright (c) 2011, 2012, 2014, Aaron Madison
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.  
 
 from django.conf import settings
 from django.forms import widgets
@@ -23,5 +32,5 @@ class GoogleMapsAddressWidget(widgets.TextInput):
         if value is None:
             value = ''
         final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
-        html = u'<input%s /><div class="map_canvas_wrapper"><div id="map_canvas"></div></div>'
+        html = '<input%s /><div class="map_canvas_wrapper"><div id="map_canvas"></div></div>'
         return mark_safe(html % flatatt(final_attrs))
