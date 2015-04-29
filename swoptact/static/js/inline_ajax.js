OpenTechStrategies/streetcrm

commit 60cd2df8885ae864cb356902e7f7cdd82f0a3eee

Update sample data.
Correct sample data (now that a participant address has been returned to a
foreignkey field) and fix a display bug introduced in commit 3a8b5ab.

swoptact/static/css/swoptact.css
@@ -217,3 +217,7 @@ input[name="_addanother"],input[name="_continue"],input[name="_saveasnew"]{
 input[type="checkbox"] {
     font-size: 1em;
 }
+/* Hide Today link */
+.datetimeshortcuts a:not(#calendarlink0) {
+    display:none;
+}

swoptact/static/js/inline_ajax.js
@@ -481,7 +481,3 @@ function setupParticipantCallbacks() {
 setupParticipantCallbacks();
 
 
-
-// getAttendees(); 
-// getAvailableParticipants();
-//saveNewPerson();

swoptact/admin.py
@@ -63,11 +63,11 @@ class SignInSheetAdminMixin(object):
 
 class ParticipantAdmin(SignInSheetAdminMixin, admin.ModelAdmin):
     """ Admin UI for participant including listing event history """
-    list_display = ("name", "phone_number",  "institution", "address",)
+    list_display = ("name", "primary_phone",  "institution", "address",)
     readonly_fields = ("event_history", "event_history_name", )
     fieldsets = (
         (None, {
-            "fields": ("first_name", "last_name", "phone_number", "institution", "secondary_phone", "email",
+            "fields": ("first_name", "last_name", "primary_phone", "institution", "secondary_phone", "email",
                         "address")
         }),
         ("Personal Event History", {

swoptact/migrations/0026_auto_20150429_1209.py
@@ -1,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('swoptact', '0025_auto_20150424_1026'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='participant',
+            old_name='phone_number',
+            new_name='primary_phone',
+        ),
+    ]

swoptact/migrations/0027_auto_20150429_1222.py
@@ -1,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('swoptact', '0026_auto_20150429_1209'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='event',
+            old_name='terminal_event',
+            new_name='major_action',
+        ),
+        migrations.AlterField(
+            model_name='event',
+            name='is_prep',
+            field=models.BooleanField(default=False, verbose_name='This meeting is part of a major action'),
+        ),
+    ]

swoptact/migrations/0028_auto_20150429_1248.py
@@ -1,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('swoptact', '0027_auto_20150429_1222'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='event',
+            name='time',
+            field=models.TimeField(blank=True, null=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='event',
+            name='date',
+            field=models.DateField(blank=True, null=True),
+        ),
+        migrations.AlterField(
+            model_name='event',
+            name='is_prep',
+            field=models.BooleanField(default=False, verbose_name='This meeting is part of a major action:'),
+        ),
+    ]

swoptact/models.py
@@ -99,11 +99,15 @@ class Address(models.Model, SerializeableMixin):
         self.DICT_DIRECTIONS = dict(self.DIRECTIONS)
 
     def __str__(self):
-        return "{number} {direction} {name} {type}".format(
+        return "{number} {direction} {name} {type} #{apartment} {city}, {state} {zipcode}".format(
             number=self.number,
             direction=self.DICT_DIRECTIONS[self.direction],
             name=self.name,
             type=self.DICT_TYPES[self.type],
+            apartment = self.apartment,
+            city = self.city,
+            state = self.state,
+            zipcode = self.zipcode
         )
 
 class Institution(models.Model, SerializeableMixin):
@@ -118,7 +122,7 @@ class Participant(models.Model, SerializeableMixin):
     """ Representation of a person who can participate in a Event """
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    phone_number = modelfields.PhoneNumberField(null=True, blank=True)
+    primary_phone = modelfields.PhoneNumberField(null=True, blank=True)
     secondary_phone = modelfields.PhoneNumberField(null=True, blank=True)
     email = models.EmailField(blank=True)
     address = models.ForeignKey(Address, null=True, blank=True)
@@ -144,14 +148,22 @@ class Participant(models.Model, SerializeableMixin):
 
 
 class Event(models.Model, mixins.AdminURLMixin, SerializeableMixin):
+    TIMES = (
+        ("10am", "Morning"),
+        ("12pm", "Noon"),
+        ("3pm", "Afternoon"),
+        ("7pm", "Evening"),
+    )
 
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255, null=True, blank=True)
-    date = models.DateTimeField(blank=True)
+    date = models.DateField(null=True, blank=True)
+    time = models.CharField(choices = TIMES, max_length = 20, null=True, blank=True)
     location = models.CharField(max_length=255, blank=True)
     participants = models.ManyToManyField(Participant, blank=True)
-    is_prep = models.BooleanField(default=False, blank=True)
-    terminal_event = models.ForeignKey("self", null=True, blank=True)
+    is_prep = models.BooleanField(default=False, blank=True, 
+                                  verbose_name = "This meeting is part of a major action:")
+    major_action = models.ForeignKey("self", null=True, blank=True)
 
     def __str__(self):
         return self.name

swoptact/static/js/event_changes.js
@@ -1,0 +1,18 @@
+/*
+This page creates a jquery calendar for 
+*/
+function testFcn() {
+    $('#id_is_prep').on(
+        "click",
+ 	function(){
+            $( ".row" ).last().toggle();
+        });
+
+    $(document).ready(function () {
+ 	$( ".row" ).last().hide();
+    });
+
+
+}
+
+testFcn();

commit 442641b138a4e4aa8f61962782aa8ce0aba6d06f

#13, update UI.
This also fixes some mistakes introduced in the previous commit 60cd2df.
Changing the name of the phone_number field required making some changes in
inline_ajax.js and views.py.  Other than that these changes are all UI updates
requested at our last demo of #13.

swoptact/static/css/swoptact.css
@@ -221,3 +221,11 @@ input[type="checkbox"] {
 .datetimeshortcuts a:not(#calendarlink0) {
     display:none;
 }
+/* make the text inside the inputs (esp. on the sign-in sheet) larger and more
+readable, without making the inputs themselves enormous */
+input {
+    font-size: large;
+}
+input.vTextField {
+    width: 200px;
+}

swoptact/static/js/inline_ajax.js
@@ -174,15 +174,10 @@ function fillStaticRow(row, participant) {
     } else {
         appendSimpleText("");
     }
-    appendSimpleText(participant.phone_number);
-    if (participant.address) {
-        appendSimpleText(participant.address.__str__);
-    } else {
-        appendSimpleText("");
-    }
+    appendSimpleText(participant.primary_phone);
 
     // Now append the buttons...
-    row.append('<td><button type="submit" class="btn btn-info participant-edit" name="_edit">✎ Edit</button> <button type="submit" class="btn btn-danger participant-unlink" name="_unlink">✘ Unlink</button></td>');
+    row.append('<td><button type="submit" class="btn btn-info participant-edit" name="_edit">✎ Edit</button> <button type="submit" class="btn btn-danger participant-unlink" name="_unlink">✘ Undo</button></td>');
 }
 
 function fillEditRow(row, participant) {
@@ -206,12 +201,7 @@ function fillEditRow(row, participant) {
     } else {
         appendSimpleTextField("");
     }
-    appendSimpleTextField(participant.phone_number);
-    if (participant.address) {
-        appendSimpleTextField(participant.address.__str__);
-    } else {
-        appendSimpleTextField("");
-    }
+    appendSimpleTextField(participant.primary_phone);
 
     // Now append the buttons...
     row.append('<td><button type="submit" class="btn btn-success participant-save" name="_save">✓ Save</button> <button type="submit" class="btn btn-warning participant-cancel" name="_cancel">✗ Cancel</button></td>');
@@ -353,8 +343,7 @@ function updateParticipant(participant){
     // UI columns stay the same
     participant.first_name = text_inputs[0].value;
     participant.last_name = text_inputs[1].value;
-    participant.phone_number = text_inputs[3].value;
-    participant.address = text_inputs[4].value;
+    participant.primary_phone = text_inputs[3].value;
     return participant;
 }
 

swoptact/templates/admin/inline_event_participants.html
@@ -13,11 +13,10 @@
         <table class="table table-striped table-bordered" id="participant-table">
           <thead>
             <tr>
-              <th>First name</th>
-              <th>Last name</th>
+              <th>First Name</th>
+              <th>Last Name</th>
                 <th>Institution</th>
-              <th>Attendee's Phone number</th>
-              <th>Attendee's Address</th>
+              <th>Attendee's Phone Number</th>
               <th>Actions</th>
             </tr>
           </thead>

swoptact/views.py
@@ -163,7 +163,7 @@ class ParticipantAPI(APIMixin, generic.UpdateView):
     """
 
     model = models.Participant
-    fields = ["id", "first_name", "last_name", "phone_number", "secondary_phone",
+    fields = ["id", "first_name", "last_name", "primary_phone", "secondary_phone",
               "email", "address"]
 
     def get(self, request, *args, **kwargs):
@@ -182,7 +182,7 @@ class CreateParticipantAPI(APIMixin, generic.CreateView):
     """
 
     model = models.Participant
-    fields = ["first_name", "last_name", "phone_number", "secondary_phone",
+    fields = ["first_name", "last_name", "primary_phone", "secondary_phone",
               "email", "address"]
 
     def form_valid(self, form, *args, **kwargs):