186fd4a504756f2dc8935006b7e86008ad5078dd
Jessica Tallon <jessica@megworld.co.uk>
2015-04-30 18:12:49 +0200

    #21 - Add migration to add groups by default

--- swoptact/admin.py
+++ swoptact/admin.py
@@ -13,6 +13,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from django import template
 from django.contrib import admin, staticfiles
 from django.contrib.admin.options import InlineModelAdmin
