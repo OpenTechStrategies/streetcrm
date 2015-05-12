a1d21f2b281915bc4e7aa4e2e3ce849de3189b4e
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2015-05-12 10:33:14 -0500

    #13, UI changes
    Add a new "address" row, and change button colors.

--- swoptact/static/js/event_changes.js
+++ swoptact/static/js/event_changes.js
@@ -5,16 +5,15 @@ function testFcn() {
     $('#id_is_prep').on(
         "click",
  	function(){
-            $( ".row" ).eq(-2).toggle();
+            $( ".row" ).eq(-1).toggle();
         });
 
     $(document).ready(function () {
         if ($('#id_is_prep').prop('checked')){
-            console.log($( ".row" ).eq(-2).value);
-            $( ".row" ).eq(-2).show();
+            $( ".row" ).eq(-1).show();
         }
         else{
-            $( ".row" ).eq(-2).hide();
+            $( ".row" ).eq(-1).hide();
         }
     });
 
