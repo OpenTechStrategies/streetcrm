9e4283efba77c6311d22284169c516bb6de928b9
Christopher Allan Webber <cwebber@dustycloud.org>
2015-06-09 14:49:24 -0500

    removing the unused createAutoComplete code

--- swoptact/static/js/inline_ajax.js
+++ swoptact/static/js/inline_ajax.js
@@ -431,29 +431,6 @@ function saveParticipant(participant_id) {
     }
 }
 
-
-function createAutocomplete(user){
-    $.get('/api/users/'+user+'/available-tags/',
-        function (tags) {
-            var source_array = [];
-            for (i = 0; i < tags.available.length; i++){
-                source_array.push({label: tags.available[i].name, value: tags.available[i].id});
-            }
-            $("#tag_choices").autocomplete({
-                source: source_array,
-                select: function(event, ui) {
-                    event.preventDefault();
-                    $("#tag_choices").val(ui.item.label);
-                    //but we need to save the value as a fk
-                },
-                focus: function(event, ui) {
-                    event.preventDefault();
-                    $("#tag_choices").val(ui.item.label);
-                }
-            });
-        }, 'json');
-}
-
 function setupParticipantCallbacks() {
     $("#participant-table").on(
         "click", "button.participant-edit",
@@ -519,6 +496,3 @@ function setupParticipantCallbacks() {
 
 
 setupParticipantCallbacks();
-
-
-createAutocomplete(1);
