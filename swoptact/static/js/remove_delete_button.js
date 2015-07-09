5e2dbaf94968f2233093d39cfa08ffc27ac11666
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2015-07-09 13:31:35 -0500

    #108: Add "Save Changes" button to action fieldset
    We had hidden the "Save and stay on page" button on all pages (see
    commit d918a35) but I'm showing it on the Action page so that there is
    an option to save the page without scrolling all the way past the
    sign-in sheet.  I've displayed the button, moved it up into the first
    fieldset (white box), and altered the styling to match the "Done"
    button.

--- swoptact/static/js/remove_delete_button.js
+++ swoptact/static/js/remove_delete_button.js
@@ -2,8 +2,17 @@
 // This is SWOP's request, see https://github.com/OpenTechStrategies/swoptact/issues/52
 
 function removeDefaultButtons() {
+    // remove the delete button
     $("div.form-actions div.pull-left a.deletelink").remove();
-    $("div#form-actions div.pull-right ").remove();
+    // remove the default "Done" button.  This was replaced by a new
+    // button in templates/admin/inline_event_participants.html that
+    // saves open rows and submits the form.
+    $("div#form-actions div.pull-right input[name='_save'] ").remove();
+    // add a "save and stay on page" button after the first fieldset
+    $("div#form-actions div.pull-right ").appendTo("#fieldset-1");
+    $("input[name='_continue']").attr("style", "display: block;");
+    $("input[name='_continue']").attr("value", "Save Changes");
+    $("input[name='_continue']").attr("class", "btn-primary btn default");
 }
 
 $(document).ready(function () {
