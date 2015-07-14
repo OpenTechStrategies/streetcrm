30388311dcd9745ff16d75fa1e16910caf8c747b
Jessica Tallon <tsyesika@tsyesika.se>
2015-07-14 17:29:07 +0200

    Use API to check saved state of models
    This uses the API to check the state of the models that has been
    saved. This allows us in future to handle the cases of specific
    fieldsets which are saved by JS and can't be checked by comparing
    the initial form data to the current.
    
    The two problems with this approach are:
    - window.onbeforeunload needs a string or not as a return value in a
      synchronous fashion but $.get(...); works asynchronously.
    
    - How to get the API URL for the models and what to do for new models
      which don't have an explicitly defined API. This potentially could be
      fixed by adding a "expose_api" attribute to the model and if that is
      set having a new API view automatically created. The admin interface
      would then have to fill the API URL for each fieldset so the JS knows
      which API to call.

--- swoptact/static/js/warn_unsaved.js
+++ swoptact/static/js/warn_unsaved.js
@@ -1,24 +1,43 @@
-initialFormData = null;
 formSubmitted = false;
 
 function warnUnsaved() {
     // If the form is being processed, don't warn
     if (formSubmitted) {
         return;
-    }   
-
-    // Check if there are any unsaved fields and if so warn before leaving
-    if (initialFormData != $("#fieldset-1").serialize()) {
-        return "You have entered data without saving it. Are you sure you want to leave this page?";
     }
 
-    return;     
+    // Find the API URL
+    var pathname = window.location.pathname.split("/");
+
+    // Find the model name from the pathname (APIs use the plural version)
+    var modelname = pathname[1] + 's';
+
+    // Find the object ID
+    var objectID = pathname[2];
+
+    var apiURL = "/api/" + modelname + "/" + objectID + "/";
+
+    // Check if there are any unsaved fields and if so warn before leaving
+    var unsavedData = false;
+
+    $.get(apiURL, function(data) {
+        $.each(data, function (field, value) {
+            var pageValue = $("#id_" + field).val();
+            if (value != field()) {
+                unsavedData = true;
+            };
+        });
+    }, "json");
+
+    if (unsavedData) {
+        return "You have entered data without saving it. Are you sure you want to leave this page?";
+    }
 }
 
 
-$(document).ready(function () {   
+$(document).ready(function () {
     // First populate the initial form data
-    initialFormData = $("#fieldset-1").serialize(); 
+    initialFormData = $("#fieldset-1").serialize();
 
     // Set up callbacks.
     $("form").submit(function () {
