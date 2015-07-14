26cefa84d052699899dec6be7fa8606528ff2e0f
Jessica Tallon <tsyesika@tsyesika.se>
2015-07-14 17:37:05 +0200

    #107 - Fix name of file in change_form.html

--- swoptact/static/js/warn_unsaved.js
+++ swoptact/static/js/warn_unsaved.js
@@ -5,20 +5,20 @@ function warnUnsaved() {
     // If the form is being processed, don't warn
     if (formSubmitted) {
         return;
-    }   
+    }
 
     // Check if there are any unsaved fields and if so warn before leaving
     if (initialFormData != $("#fieldset-1").serialize()) {
         return "You have entered data without saving it. Are you sure you want to leave this page?";
     }
 
-    return;     
+    return;
 }
 
 
-$(document).ready(function () {   
+$(document).ready(function () {
     // First populate the initial form data
-    initialFormData = $("#fieldset-1").serialize(); 
+    initialFormData = $("#fieldset-1").serialize();
 
     // Set up callbacks.
     $("form").submit(function () {
