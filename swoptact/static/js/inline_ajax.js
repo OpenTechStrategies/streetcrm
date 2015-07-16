OpenTechStrategies/streetcrm

commit d45c713aa371e0342399992278f19ba5acdc4bf8

Add link to Institution profile from sign-in sheet.
This is a bit of a roundabout solution that builds on my work in 69f4ad8
and d42f259.  I created the `createProfileLink()` and
`getExistingInlinedModelProfileUrl()` functions in those commits, and
extend them here to take an optional url path argument.  Generally, we
want to use strings provided in inline_form_config.  This commit is an
intermediate solution so that we can have a link to an institutional
profile while I learn how to create the string for the url path in a
more consistent way.  The difficulty comes here because the Institution
is not actually the inlined model on the sign-in sheet, so I'll need to
come up with some other way to refer to it.

swoptact/static/js/inline_ajax.js
@@ -282,10 +282,17 @@ function fillExistingInlinedModelUrl(inlined_model_id) {
     return formatter({"<inlined_model_id>": inlined_model_id});
 }
 
-function getExistingInlinedModelProfileUrl(inlined_model_id) {
-    var formatter = gummyStringFormatter(
-        getInlineConfig()["existing_inlined_model_profile_url"]);
-    return formatter({"<inlined_model_id>": inlined_model_id});
+function getExistingInlinedModelProfileUrl(inlined_model_id, url_string=null) {
+    var url_for_profile = "";
+    if (url_string != null){
+        url_for_profile = url_string + inlined_model_id;
+    }
+    else{
+        var formatter = gummyStringFormatter(
+            getInlineConfig()["existing_inlined_model_profile_url"]);
+        url_for_profile = formatter({"<inlined_model_id>": inlined_model_id});
+    }
+    return url_for_profile;
 }
 
 function getNewInlinedModelUrl() {
@@ -453,10 +460,10 @@ function turnOnAttendeeAutocomplete(edit_row) {
 /*
   Create a link to the change form of an inlined model with the given ID
 */
-function createProfileLink(inlined_model_id, existing_element) {
+function createProfileLink(inlined_model_id, existing_element, url_for_profile=null) {
     var link_to_profile = $("<a/>");
     link_to_profile.html(" &#x2139;");
-    link_to_profile.attr("href", getExistingInlinedModelProfileUrl(inlined_model_id));
+    link_to_profile.attr("href", getExistingInlinedModelProfileUrl(inlined_model_id, url_for_profile));
     existing_element.append(link_to_profile);
 }
 
@@ -500,7 +507,7 @@ function fillStaticRow(row, inlined_model) {
             }
             else if (field['form_name'] == 'institution' && inlined_model.institution){
                 // this doesn't work yet because I don't know how to set the URL correctly
-                // createProfileLink(inlined_model.institution.id, new_elt);
+                createProfileLink(inlined_model.institution.id, new_elt, "/swoptact/institution/");
             }
             td_wrap = $("<td/>");
             td_wrap.attr("data-form-name", field['form_name']);

commit 0d4eec74f668111c608babf50ecd35e9417eba68

Fix inline tables on Chrome browsers
Javascript does not universally support assignment of default values to
function parameters ala "function foo (a=null)". Introduction of this
syntax in d45c713 broke inline tables (such as those seen on change
pages) in browsers where such syntax is not supported (such as
Chrome). Assignment of default values now done through old fashioned
if/then statement.

swoptact/static/js/inline_ajax.js
@@ -282,7 +282,8 @@ function fillExistingInlinedModelUrl(inlined_model_id) {
     return formatter({"<inlined_model_id>": inlined_model_id});
 }
 
-function getExistingInlinedModelProfileUrl(inlined_model_id, url_string=null) {
+function getExistingInlinedModelProfileUrl(inlined_model_id, url_string) {
+    if (url_string == undefined) { url_string=null; }
     var url_for_profile = "";
     if (url_string != null){
         url_for_profile = url_string + inlined_model_id;
@@ -460,7 +461,8 @@ function turnOnAttendeeAutocomplete(edit_row) {
 /*
   Create a link to the change form of an inlined model with the given ID
 */
-function createProfileLink(inlined_model_id, existing_element, url_for_profile=null) {
+function createProfileLink(inlined_model_id, existing_element, url_for_profile) {
+    if (url_for_profile == undefined) { url_for_profile = null; }
     var link_to_profile = $("<a/>");
     link_to_profile.html(" &#x2139;");
     link_to_profile.attr("href", getExistingInlinedModelProfileUrl(inlined_model_id, url_for_profile));