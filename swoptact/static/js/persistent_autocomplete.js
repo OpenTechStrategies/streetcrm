80aa055f426793127007a3e6584c0137a8e6e91d
Christopher Allan Webber <cwebber@dustycloud.org>
2015-09-10 17:33:52 -0500

    Issue #78: Actually update the widget's real value
    Widget works now!

--- swoptact/static/js/persistent_autocomplete.js
+++ swoptact/static/js/persistent_autocomplete.js
@@ -4,9 +4,36 @@ function setupAutoCompleteCallbacks() {
             var jq_obj = $(obj);
             var options = $.parseJSON(jq_obj.attr("data-options"));
             var option_names = Object.keys(options);
-            jq_obj.find(".user-widget").autocomplete({
+            var user_widget = jq_obj.find(".user-widget");
+            var real_val_widget = jq_obj.find(".real-value");
+
+            function maybeUpdateHiddenValue() {
+                var selected_val = options[user_widget.val()];
+                if (selected_val) {
+                    real_val_widget.val(selected_val);
+                }
+            }
+            
+
+            function updateCallback(event, ui) {
+                maybeUpdateHiddenValue();
+            }
+
+            user_widget.autocomplete({
                 source: option_names,
+                select: updateCallback,
+                focus: updateCallback,
+                close: updateCallback,
             });
+
+            // Update on keystroke too
+            user_widget.on(
+                "keyup",
+                function (event) {
+                    // 13 is enter key, and we have a different handler for that
+                    if (event.which != 13) {
+                        maybeUpdateHiddenValue();
+                    }});
         });
 }
 
