9d290bf493e2f8e97c6e8c2b2cdc3ce89486789f
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2015-09-11 09:23:54 -0500

    Remove deprecated TagSkipping form: #78
    We removed this form, which is no longer used, in e2d743e, but a couple
    references remained and were causing errors.

--- swoptact/admin.py
+++ swoptact/admin.py
@@ -421,7 +421,6 @@ class EventAdmin(mixins.AdminArchiveMixin, AjaxyInlineAdmin):
     form = st_forms.autocomplete_modelform_factory(
         model=models.Event,
         exclude=("participants", "archived"),
-        form=st_forms.TagSkippingAutoCompleteModelForm,
     )
     actions = None
     inline_form_config = {
@@ -475,7 +474,6 @@ class InstitutionAdmin(mixins.AdminArchiveMixin, AjaxyInlineAdmin):
     form = st_forms.autocomplete_modelform_factory(
         model=models.Institution,
         exclude=("archived", "contacts",),
-        form=st_forms.TagSkippingAutoCompleteModelForm,
     )
     actions = None
     change_list_template = "admin/change_list_without_header.html"
