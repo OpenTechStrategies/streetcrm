75837f3b1f467950ac62651fe92f075b07ac5599
nttaylor <noeltaylo@gmail.com>
2015-09-17 16:10:53 -0500

    Tweak hiding/showing scripts #192
    "event_changes.js" now finds the row that shows the associated major
    action by its id instead of by assuming it is the last row in the
    table. The earlier design caused final rows to disappear while
    experimenting with moving the "archive" and "done" buttons to a position
    below the first fieldset. Relatedly, "remove_delete_button" uses a more
    streamlined approach to achieving the desired behavior.

--- swoptact/static/js/remove_delete_button.js
+++ swoptact/static/js/remove_delete_button.js
@@ -1,21 +1,17 @@
 // remove the delete button from the lower left side of pages
 // This is SWOP's request, see https://github.com/OpenTechStrategies/swoptact/issues/52
 
-function removeDefaultButtons() {
+$(document).ready(function () {
+    // Move "form-actions" to a position under the first fieldset. We
+    // don't need it after ajax-generated table at the bottom of the
+    // form, where saves are done immediately after editing within the
+    // table cell.
+    $("#form-actions").appendTo("#fieldset-1");
+
+    // Hide the left-hand button
+    $("#form-actions .pull-left").hide();
     // Hide all right-hand inputs for a second
     $("#form-actions input").hide();
     // Make some changes to the "save and stay" button that we're interested in
     $("#form-actions input[name='_continue']").addClass("btn-primary btn default").val("Save Changes").show();
-
-    // Move the right-hand button set to a position after the first fieldset.
-    // We don't need it after ajax-generated table at the bottom of the form,
-    // where saves are done immediately after editing within the table cell.
-    $("#form-actions .pull-right").appendTo("#fieldset-1");
-    
-    // Remove the original "form-actions";
-    $("#form-actions").remove();
-}
-
-$(document).ready(function () {
-    removeDefaultButtons();
 });
