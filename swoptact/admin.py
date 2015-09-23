6d462b98bb4dd7cb2a4b6ec0279faddf889bf906
Christopher Allan Webber <cwebber@dustycloud.org>
2015-09-23 17:49:43 -0500

    And cancel out that filter_horizontal

--- swoptact/admin.py
+++ swoptact/admin.py
@@ -545,6 +545,7 @@ class UserAdmin(auth.admin.UserAdmin):
     list_display = ("username", "email", "first_name", "last_name")
     list_filter = ("is_superuser", "is_active", "groups")
     actions = None
+    filter_horizontal = tuple()
     filter_vertical = ('groups', 'user_permissions',)
 
 
