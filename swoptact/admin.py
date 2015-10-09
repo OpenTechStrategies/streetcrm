29b2a10c6af74733d6b0a0e2a06b46d458decec5
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2015-10-09 13:41:12 -0500

    Subclass list_filter for #250
    Add another subclass to SimpleListFilter for tags, just as @xray7224 did
    for the ArchivedFilter.  Alter that ArchivedFilter subclass to include
    counts.
    
    This doesn't quite finish #250, because I still have to manage the
    filters on the User list.  I probably need to write another subclass for
    the group filter.

--- swoptact/admin.py
+++ swoptact/admin.py
@@ -770,7 +770,7 @@ class AjaxyInlineAdmin(watson.SearchAdmin):
 
 class EventAdmin(mixins.AdminArchiveMixin, AjaxyInlineAdmin):
     search_fields = ("name", "location")
-    list_filter = (admin_filters.ArchiveFilter, "tags__name")
+    list_filter = (admin_filters.TagFilter,)
     list_display = ("name", "location", "date", "attendee_count",)
     change_form_template = "admin/event_change_form.html"
     form = st_forms.autocomplete_modelform_factory(
@@ -824,7 +824,7 @@ class EventAdmin(mixins.AdminArchiveMixin, AjaxyInlineAdmin):
 class InstitutionAdmin(mixins.AdminArchiveMixin, AjaxyInlineAdmin):
     search_filter = ("name", "inst_street_address", "inst_city_address", "inst_state_address"
                      "inst_zipcode_address")
-    list_filter = (admin_filters.ArchiveFilter, "tags__name")
+    list_filter = (admin_filters.ArchiveFilter, admin_filters.TagFilter,)
     list_display = ("name", )
     change_form_template = "admin/ajax_inline_change_form.html"
     form = st_forms.autocomplete_modelform_factory(
--- swoptact/admin_filters.py
+++ swoptact/admin_filters.py
@@ -1,16 +1,20 @@
 
 from django.contrib.admin import SimpleListFilter
 from django.utils.translation import ugettext_lazy as _
+from swoptact.models import Tag
 
 class ArchiveFilter(SimpleListFilter):
     title = _("Archive Status")
     parameter_name = "archive_status"
 
     def lookups(self, request, model_admin):
+        active_count = str(model_admin.model.objects.filter(archived__isnull=True).count())
+        archived_count = str(model_admin.model.objects.exclude(archived__isnull=True).count())
+        all_count = str(model_admin.model.objects.all().count())
         return (
-            (None, _("Active")),
-            ("archived", _("Archived")),
-            ("all", _("All")),
+            (None, _("Active" + " (" + active_count + ")")),
+            ("archived", _("Archived"  + " (" + archived_count + ")")),
+            ("all", _("All"  + " (" + all_count + ")")),
         )
 
     def queryset(self, request, queryset):
@@ -30,3 +34,39 @@ class ArchiveFilter(SimpleListFilter):
                 }, []),
                 'display': title,
             }
+
+
+class TagFilter(SimpleListFilter):
+    title = _("Tag")
+    parameter_name = "tags__name"
+
+    def lookups(self, request, model_admin):
+        tag_set = Tag.objects.all().order_by("name")
+        all_count = str(model_admin.model.objects.all().count())
+        choices_set = [("all", _("All"  + " (" + all_count + ")")),]
+        for name in tag_set:
+            count = str(model_admin.model.objects.filter(tags__name=name).count())
+            choices_set.append((name, _(str(name) + " (" + count + ")")))
+        return choices_set
+            
+    def queryset(self, request, queryset):
+        if not self.used_parameters:
+            self.used_parameters = {'tags__name': 'all'}
+        try:
+            if self.used_parameters["tags__name"] == 'all':
+                return queryset
+            else:
+                return queryset.filter(**self.used_parameters)
+        except ValidationError as e:
+            raise IncorrectLookupParameters(e)
+
+    def choices(self, cl):
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': self.value() == lookup,
+                'query_string': cl.get_query_string({
+                    self.parameter_name: lookup,
+                }, []),
+                'display': title,
+            }
+            
