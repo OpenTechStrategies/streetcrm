8c443a459c73cdf06ff2ff49d6b9755d042cb3ba
pjsier <pjsier@gmail.com>
2017-10-26 13:03:44 -0500

    Clear and update toast message timeouts

--- streetcrm/static/js/toast_message.js
+++ streetcrm/static/js/toast_message.js
@@ -3,6 +3,9 @@
  * from the supplied message
 */
 
+// Setting variable to hold the timeout ID so it can be cleared
+var TOAST_TIMEOUT;
+
 /*
  * Splitting removal function so it can be added as a click handler
  * if someone wants to remove a message immediately.
@@ -16,10 +19,12 @@ function removeToastMessage() {
  * Disappears after 3 seconds or on click.
  */
 function toastMessage(message) {
+    if (TOAST_TIMEOUT) clearTimeout(TOAST_TIMEOUT);
+    $("#toast").remove();
     $("body").append("<div id='toast'>" + message + "</div>");
     $("#toast").fadeIn(500);
     $("#toast").on("click", removeToastMessage);
-    setTimeout(function () {
+    TOAST_TIMEOUT = setTimeout(function () {
         $("#toast").fadeOut(500, function () { $("#toast").remove(); });
     }, 3000);
 }
